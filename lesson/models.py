8b82858008f3928ec3bd8ede8f20bff2438d1618
arun.a.sohdi+github@gmail.com <arun.a.sohdi+github@gmail.com>
2020-06-04 19:37:51 +0100

    Added new app and updated lesson pages

--- lesson/models.py
+++ lesson/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, RichTextFieldPanel, InlinePanel, PageChooserPanel
 from wagtail.core.models import Page, Orderable
 
 # Create your models here.
 
 class LessonPage(Page):
     
-    templates = 'subject/subject_landing_page.html'
-    parent_page_types = ["subject.SubjectLandingPage"]
+    templates = 'lesson/lesson_page.html'
+    parent_page_types = ["subject.SubjectLandingPage","subject.SubjectKSLandingPage"]
     
-    lesson_name = models.CharField(max_length = 150, null=True, blank= True)
+    lesson_name = models.CharField(
+        max_length = 150,
+        null=True,
+        blank= True
+    )
+    
+    presentation_link = models.URLField(max_length=200,blank=True,null=True)
+    
+    content_panels  = Page.content_panels + [
+        FieldPanel("lesson_name"),
+        FieldPanel("presentation_link")        
+    ]
     
